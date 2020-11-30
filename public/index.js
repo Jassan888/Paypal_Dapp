4a1c3ee8532e59fbe2015d751ef3b9f8dbfc7b74
jeffrey <jeffreyassan88@gmail.com>
2020-11-29 23:34:48 -0500

    added axios

--- public/index.js
+++ public/index.js
@@ -3,7 +3,9 @@ class App extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            'total amount': 1000
+            'total amount': 1000,
+            'amount': 100,
+            'email': ''
         }
     }
     render(){
@@ -14,8 +16,9 @@ class App extends React.Component{
                     <p>Total lottery amount is {this.state.total_amount} </p>
                 </div>
                 <form>
-                    <input placeHolder= 'amount'/>
-                    <input placeHolder= 'email'/>
+                    <input placeHolder= 'amount' value= {this.state.total_amount}
+                    onCghange= {event=> this.setState({amount: event.traget.value})}/>
+                    <input placeHolder= 'email'value= {this.state.email}/>
                     <button>Participate </button>
                 </form>
             </div>
