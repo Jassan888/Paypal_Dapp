d141fdf677463a915a941124cee50d8e9fc1bb38
jeffrey <jeffreyassan88@gmail.com>
2020-11-29 22:39:15 -0500

    update index.js

--- public/index.js
+++ public/index.js
@@ -1,3 +1,16 @@
 class App extends React.Component{
-    
-}
+    render(){
+        return(
+            <div>
+                <h1>Lottery Appication </h1>
+            </div>
+        )
+    }
+};
+
+ReactDOM.render(
+    <div>
+        <App />
+    </div>
+    , document.getElementsById('reactBinding')
+);
