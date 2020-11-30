5ca54ea5b696ff068b965aa1dde3c7c7cd49c8ef
jeffrey <jeffreyassan88@gmail.com>
2020-11-29 22:41:28 -0500

    update index.js

--- public/index.js
+++ public/index.js
@@ -1,8 +1,23 @@
 class App extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            'total amount': 1000
+        }
+    }
     render(){
         return(
             <div>
                 <h1>Lottery Appication </h1>
+                <div>
+                    <p>Total lottery amount is {this.state.total_amount} </p>
+                </div>
+                <form>
+                    <input placeHolder= 'amount'/>
+                    <input placeHolder= 'email'/>
+                    <button>Participate </button>
+                </form>
             </div>
         )
     }
